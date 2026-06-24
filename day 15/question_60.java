38e1d6f28dff23bbabd12a249e3bd21dfd355268
Sanay Prabhat <89979329+sanayprabhat@users.noreply.github.com>
2026-06-24 21:29:43 +0530

    Add files via upload

---
+++ day 15/question_60.java
@@ -0,0 +1,33 @@
+
+import java.util.Scanner;
+
+public class question_60 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter array size: ");
+        int n = sc.nextInt();
+
+        int[] arr = new int[n];
+        System.out.println("Enter array elements:");
+        for (int i = 0; i < n; i++) {
+            arr[i] = sc.nextInt();
+        }
+
+        int index = 0;
+        for (int i = 0; i < n; i++) {
+            if (arr[i] != 0) {
+                arr[index] = arr[i];
+                index++;
+            }
+        }
+        while (index < n) {
+            arr[index] = 0;
+            index++;
+        }
+        System.out.println("Array after moving zeroes to end:");
+        for (int i = 0; i < n; i++) {
+            System.out.print(arr[i] + " ");
+        }
+    }    
+}
